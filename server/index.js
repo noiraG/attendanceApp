65baa9b4dfff9a647f9328089705535cc0c294a8
csyviroy <csyviroy@gmail.com>
2019-09-24 16:16:45 +0800

    insert documentation for reading

--- server/index.js
+++ server/index.js
@@ -41,6 +41,22 @@ app.post("/face", (req, res) => {
 
 //login - retrieve user based on username and password
 app.post("/login", (req, res) => {
+  /* note:
+    This function require the front end to POST req's body with:
+    1. username
+    2. password
+    If record matched, the detail of the user will be send back:
+    1. candidate: {
+      referenceKey,
+      matriculationNo,
+      name,
+      password,
+      username
+    }
+    else:
+    1. send back false
+  */
+
   ref
     .child("users")
     .orderByChild("username")
@@ -74,6 +90,27 @@ app.post("/login", (req, res) => {
 
 //View all student, return all students
 app.get("/api/student", (req, res) => {
+  /* note:
+    This function require the front end to get the api:
+      it will return with a list of student:
+      studentlist: [
+        {
+          key,
+          value: {
+            matriculationNo,
+            name,
+            password,
+            role,
+            username
+          }
+        },
+        {
+          key,
+          value
+        }
+      ]
+  */
+
   var studentlist = [];
   ref.once("value", function(snapshot) {
     var users = snapshot.val().users;
@@ -92,17 +129,52 @@ app.get("/api/student", (req, res) => {
 //View 1 student, return 1 specific student
 //url need to change, include reference key
 app.post("/api/student/one", (req, res) => {
+  /* note:
+    This function require the front end to POST req's body with:
+      1. matriculationNo
+    If record matched, the detail of the user will be send back:
+      student: 
+      {
+        key,
+        value: {
+          matriculationNo,
+          name,
+          password,
+          role,
+          username
+        }
+      }
+  */
+
   ref
     .child("users")
     .orderByChild("matriculationNo")
     .equalTo(req.body.matriculationNo)
     .once("value", function(snapshot) {
-      res.send(snapshot.val());
+      if (snapshot.exists()) {
+        var users = snapshot.val();
+        Object.keys(users).forEach(k => {
+          student = { key: k, value: users[k] };
+          res.send(student);
+        });
+      } else {
+        res.send(false);
+      }
     });
 });
 
 //update a specific user based on the reference key
 app.post("/api/student/update", (req, res) => {
+  /* note:
+    This function require the front end to POST req's body with:
+      1. referenceKey of specific student
+      2. matriculationNo
+      3. name
+      4. username
+      5. password
+    If record updated, send back true
+  */
+
   var newData = {
     matriculationNo: req.body.matriculationNo,
     name: req.body.name,
@@ -123,6 +195,12 @@ app.post("/api/student/update", (req, res) => {
 
 //delete a specific student account based on referenceKey
 app.post("/api/student/delete", (req, res) => {
+  /* note:
+    This function require the front end to POST req's body with:
+      1. referenceKey of specific student
+    If record is removed, send back true
+  */
+
   ref
     .child("users")
     .child(req.body.referenceKey)
@@ -137,6 +215,18 @@ app.post("/api/student/delete", (req, res) => {
 
 //Add student account
 app.post("/api/student/add/", (req, res) => {
+  /* note:
+    This function require the front end to POST req's body with:
+      1. matriculationNo
+      2. name
+      3. username
+      4. password
+    The function will check if the user matriculationNo existed before:
+      If yes, send back false
+      else, add the student account
+        If record is added, send back true
+  */
+
   parameterList = req.body;
   //check if user existed
   ref
@@ -165,6 +255,27 @@ app.post("/api/student/add/", (req, res) => {
 
 //View all class, return all class
 app.get("/api/class", (req, res) => {
+  /* note:
+    This function require the front end to get the api:
+      it will return with a list of class based on today date:
+        classSession: [
+                {
+                  key,
+                  value: {
+                    matriculationNo,
+                    name,
+                    password,
+                    role,
+                    username
+                  }
+                },
+                {
+                  key,
+                  value
+                }
+              ]
+  */
+
   var classSession = [];
   ref
     .child("class")
@@ -182,9 +293,23 @@ app.get("/api/class", (req, res) => {
     });
 });
 
-//Need testing
 //Add class session
 app.post("/api/class/add/", (req, res) => {
+  /* note:
+    This function require the front end to POST req's body with:
+      1. array of student matriculationNo
+      2. startingTime
+      3. endingTime
+      4. classIndex
+      5. courseIndex
+      6. courseName
+      7. supervisor
+    The function will check if the user class existed before:
+      If yes, send back false
+      else, add the class session
+        If record is added, then add attendance record of students taking the class
+  */
+
   incr = 0;
   parameterList = req.body.class;
   matriculationNo = req.body.matriculationNo;
@@ -274,8 +399,16 @@ app.post("/api/class/add/", (req, res) => {
     });
 });
 
-//delete a specific student account based on referenceKey
+//delete a specific class session based on referenceKey
 app.post("/api/class/delete", (req, res) => {
+  /* note:
+    This function require the front end to POST req's body with:
+      1. referenceKey of specific class
+    If record is removed, 
+      then remove attendance record of the respective class session
+      send back true
+  */
+
   console.log(req.body.referenceKey);
   var i;
 
@@ -291,10 +424,23 @@ app.post("/api/class/delete", (req, res) => {
           .child(req.body.referenceKey + i.toString())
           .remove();
       }
+      res.send(true);
     });
 });
 
+//update attendance status to attended based on classDetail and matriculationNo of the student
 app.post("/api/attendance/update", (req, res) => {
+  /* note:
+    This function require the front end to POST req's body with:
+      1. classDetail
+        a. courseIndex
+        b. classIndex
+      2. matriculationNo
+    This function will find the class reference key based on courseIndex and classIndex 
+      then update the attendance record of the specific student in the class session
+      send back true
+  */
+
   console.log(req.body.classDetail);
   let classDetail = req.body.classDetail;
   let matriculationNo = req.body.matriculationNo;
