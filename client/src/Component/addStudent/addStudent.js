6fafecb9dae82a49ab56c712e325b47add6ada3e
csyviroy <csyviroy@gmail.com>
2019-10-05 19:59:56 +0800

    Got the model to work at the server side, still fixing some bug

--- client/src/Component/addStudent/addStudent.js
+++ client/src/Component/addStudent/addStudent.js
@@ -1,113 +1,151 @@
 import React from "react";
 import Webcam from "react-webcam";
 import canvas from "canvas";
-import "./styles.scss"
+import "./styles.scss";
 
-const faceapi = require('face-api.js');
+const faceapi = require("face-api.js");
 
 export default class AddStudent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            matriculationNo: "",
-            name: "",
-            username: "",
-            password: "",
-            descriptor: null,
-            image: null,
-            showCamera: true
-        };
-        this.webcamRef = React.createRef();
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      matriculationNo: "",
+      name: "",
+      username: "",
+      password: "",
+      descriptor: null,
+      image: null,
+      showCamera: true
+    };
+    this.webcamRef = React.createRef();
+  }
 
-    render() {
-        const {matriculationNo, name, username, password} = this.state
-        return (
-            <div className="student-container">
-                <div className="student-wrapper">
-                    <div className="student-form">
-                        <div className="student-form__header">Name</div>
-                        <input className="student-form__input" type="text" value={name}
-                               onChange={(e) => this.setState({name: e.currentTarget.value})}/>
-                        <div className="student-form__header">Matriculation Number</div>
-                        <input className="student-form__input" type="text" value={matriculationNo}
-                               onChange={(e) => this.setState({matriculationNo: e.currentTarget.value})}/>
-                        <div className="student-form__header">Username</div>
-                        <input className="student-form__input" type="text" value={username}
-                               onChange={(e) => this.setState({username: e.currentTarget.value})}/>
-                        <div className="student-form__header">Password</div>
-                        <input className="student-form__input" type="password" value={password}
-                               onChange={(e) => this.setState({password: e.currentTarget.value})}/>
-                    </div>
-                    <div className="student-camera">
-                        {this.state.showCamera ? <Webcam className="student-camera__feed"
-                                                         audio={false}
-                                                         height={300}
-                                                         ref={this.webcamRef}
-                                                         screenshotFormat="image/jpeg"
-                                                         width={500}
-                                                         videoConstraints={{
-                                                             width: 500,
-                                                             height: 300,
-                                                             facingMode: "user"
-                                                         }}
-                            /> :
-                            <img id="photo" className="student-camera__feed" src={this.state.image}/>
-                        }
-                        {this.state.showCamera ?
-                            <div className="student-camera__btn" onClick={this.webcamCapture}>Take Photo</div> :
-                            <div className="student-camera__btn" onClick={this.takeAnother}>Retake Photo</div>
-                        }
-                    </div>
-                </div>
-                <div className="student-form__btn" onClick={this.onBtnClick}>Submit</div>
-            </div>
+  render() {
+    const { matriculationNo, name, username, password } = this.state;
+    return (
+      <div className="student-container">
+        <div className="student-wrapper">
+          <div className="student-form">
+            <div className="student-form__header">Name</div>
+            <input
+              className="student-form__input"
+              type="text"
+              value={name}
+              onChange={e => this.setState({ name: e.currentTarget.value })}
+            />
+            <div className="student-form__header">Matriculation Number</div>
+            <input
+              className="student-form__input"
+              type="text"
+              value={matriculationNo}
+              onChange={e =>
+                this.setState({ matriculationNo: e.currentTarget.value })
+              }
+            />
+            <div className="student-form__header">Username</div>
+            <input
+              className="student-form__input"
+              type="text"
+              value={username}
+              onChange={e => this.setState({ username: e.currentTarget.value })}
+            />
+            <div className="student-form__header">Password</div>
+            <input
+              className="student-form__input"
+              type="password"
+              value={password}
+              onChange={e => this.setState({ password: e.currentTarget.value })}
+            />
+          </div>
+          <div className="student-camera">
+            {this.state.showCamera ? (
+              <Webcam
+                className="student-camera__feed"
+                audio={false}
+                height={300}
+                ref={this.webcamRef}
+                screenshotFormat="image/jpeg"
+                width={500}
+                videoConstraints={{
+                  width: 500,
+                  height: 300,
+                  facingMode: "user"
+                }}
+              />
+            ) : (
+              <img
+                id="photo"
+                className="student-camera__feed"
+                src={this.state.image}
+              />
+            )}
+            {this.state.showCamera ? (
+              <div className="student-camera__btn" onClick={this.webcamCapture}>
+                Take Photo
+              </div>
+            ) : (
+              <div className="student-camera__btn" onClick={this.takeAnother}>
+                Retake Photo
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="student-form__btn" onClick={this.onBtnClick}>
+          Submit
+        </div>
+      </div>
+    );
+  }
 
-        );
-    }
+  onBtnClick = () => {
+    console.log("clicked");
+    const { matriculationNo, name, username, password } = this.state;
+    this.generateDescriptors()
+      .then(res => {
+        console.log(res.descriptor);
+        console.log(JSON.stringify(res.descriptor));
+        fetch("http://localhost:5000/api/student/add/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            matriculationNo: matriculationNo,
+            name: name,
+            username: username,
+            password: password,
+            descriptor: res.descriptor
+          })
+        })
+          .then(res => res.json())
+          .then(res => console.log(res));
+      })
+      .catch(e => console.log(e));
+  };
 
+  webcamCapture = async () => {
+    const imageSrc = this.webcamRef.current.getScreenshot();
+    this.setState({ image: imageSrc, showCamera: false });
+  };
 
-    onBtnClick = () => {
-        const {matriculationNo, name, username, password} = this.state;
-        this.generateDescriptors().then(
-            (res) => {
-                console.log( JSON.stringify(res.descriptor))
-                fetch("http://localhost:5000/api/student/add/", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        matriculationNo: matriculationNo,
-                        name: name,
-                        username: username,
-                        password: password,
-                        descriptor: res.descriptor
-                    }),
-                }).then((res) => res.json()).then((res) => console.log(res));
-            }).catch(e => console.log(e))
+  generateDescriptors = async () => {
+    await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
+    await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
+    await faceapi.nets.faceRecognitionNet.loadFromUri("/models");
+    await faceapi.nets.faceExpressionNet.loadFromUri("/models");
+    // let image = document.getElementById("photo");
+    let result = await faceapi
+      .detectSingleFace(
+        "photo",
+        new faceapi.SsdMobilenetv1Options({ minConfidence: 0.5 })
+      )
+      .withFaceLandmarks()
+      .withFaceExpressions()
+      .withFaceDescriptor();
+    return result;
+  };
 
-    }
-
-    webcamCapture = async () => {
-        const imageSrc = this.webcamRef.current.getScreenshot();
-        this.setState({image: imageSrc, showCamera: false});
-    }
-
-    generateDescriptors = async () => {
-        await faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-        await faceapi.nets.faceLandmark68Net.loadFromUri('/models')
-        await faceapi.nets.faceRecognitionNet.loadFromUri('/models')
-        await faceapi.nets.faceExpressionNet.loadFromUri('/models')
-        // let image = document.getElementById("photo");
-        let result = await faceapi.detectSingleFace("photo", new faceapi.SsdMobilenetv1Options({minConfidence: 0.5}))
-            .withFaceLandmarks()
-            .withFaceExpressions()
-            .withFaceDescriptor()
-        return result
-    }
-
-    takeAnother = () => {
-        this.setState({showCamera: true});
-    }
+  takeAnother = () => {
+    this.setState({ showCamera: true });
+  };
 }
--- client/src/Component/takeAttendance/takeAttendance.js
+++ client/src/Component/takeAttendance/takeAttendance.js
@@ -1,52 +1,113 @@
 import React from "react";
 import "./styles.scss";
-import Webcam from "react-webcam"
+import Webcam from "react-webcam";
+import canvas from "canvas";
+
+const faceapi = require("face-api.js");
 
 class TakeAttendance extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            course: "",
-            image: null,
-            showCamera: true
-        };
-        this.webcamRef = React.createRef();
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      course: "",
+      image: null,
+      showCamera: true
+    };
+    this.webcamRef = React.createRef();
+  }
 
-    render() {
-        return (
-            <div className="attendance-container">
-                <div className="attendance-header">Input your Course Code</div>
-                <input className="attendance-input" type="text" value={this.state.course} onChange={(e) => this.setState({course: e.target.value})}/>
-                {this.state.showCamera ? <Webcam
-                        audio={false}
-                        height={300}
-                        ref={this.webcamRef}
-                        screenshotFormat="image/jpeg"
-                        width={500}
-                        videoConstraints={{width: 500, height: 300, facingMode: "user"}}
-                    /> :
-                    <img src={this.state.image}/>
-                }
-                {this.state.showCamera ? <div className="camera-btn" onClick={this.webcamCapture}>Take Photo</div> :
-                    <div className="camera-btn" onClick={this.takeAnother}>Retake Photo</div>
-                }
-                <div className="attendance-btn">Submit</div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="attendance-container">
+        <div className="attendance-header">Input your Course Code</div>
+        <input
+          className="attendance-input"
+          type="text"
+          value={this.state.course}
+          onChange={e => this.setState({ course: e.target.value })}
+        />
+        {this.state.showCamera ? (
+          <Webcam
+            audio={false}
+            height={300}
+            ref={this.webcamRef}
+            screenshotFormat="image/jpeg"
+            width={500}
+            videoConstraints={{ width: 500, height: 300, facingMode: "user" }}
+          />
+        ) : (
+          <img id="photo" src={this.state.image} />
+        )}
+        {this.state.showCamera ? (
+          <div className="camera-btn" onClick={this.webcamCapture}>
+            Take Photo
+          </div>
+        ) : (
+          <div className="camera-btn" onClick={this.takeAnother}>
+            Retake Photo
+          </div>
+        )}
+        <div className="attendance-btn" onClick={this.onBtnClick}>
+          Submit
+        </div>
+      </div>
+    );
+  }
 
-    webcamCapture = async() => {
-        const imageSrc = this.webcamRef.current.getScreenshot();
-        this.setState({image: imageSrc, showCamera: false});
-    }
+  onBtnClick = () => {
+    console.log("click");
+    const { course } = this.state;
+    this.generateDescriptors()
+      .then(res => {
+        console.log(JSON.stringify(res.descriptor));
+        fetch("http://localhost:5000/api/attendance/update/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            classDetail: {
+              courseIndex: "CZ2002",
+              classIndex: "102111"
+            },
+            descriptor: {
+              _descriptors: res.descriptor,
+              _label: "U1880216A"
+            }
+          })
+        })
+          .then(res => res.json())
+          .then(res => console.log(res));
+      })
+      .catch(e => console.log(e));
+  };
 
+  webcamCapture = async () => {
+    const imageSrc = this.webcamRef.current.getScreenshot();
+    this.setState({ image: imageSrc, showCamera: false });
+  };
 
+  generateDescriptors = async () => {
+    await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
+    await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
+    await faceapi.nets.faceRecognitionNet.loadFromUri("/models");
+    await faceapi.nets.faceExpressionNet.loadFromUri("/models");
+    // let image = document.getElementById("photo");
+    let result = await faceapi
+      .detectSingleFace(
+        "photo",
+        new faceapi.SsdMobilenetv1Options({ minConfidence: 0.5 })
+      )
+      .withFaceLandmarks()
+      .withFaceExpressions()
+      .withFaceDescriptor();
+    console.log(result);
+    return result;
+  };
 
-
-    takeAnother = () => {
-        this.setState({showCamera: true});
-    }
+  takeAnother = () => {
+    this.setState({ showCamera: true });
+  };
 }
 
 export default TakeAttendance;
