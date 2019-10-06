4839612a67cf99c6e55540336c5f50f4fafb5270
noirag <yuxuanng@hotmail.com>
2019-10-06 23:17:13 +0800

    Added alerts and admin attendance taking

--- client/src/App.js
+++ client/src/App.js
@@ -1,81 +1,86 @@
 import React from "react";
 import NavigationBar from "./Component/navigationbar/navigateBar";
 import "./App.css";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import {BrowserRouter as Router, Route, Redirect} from "react-router-dom";
 import TakeAttendance from "./Component/takeAttendance/takeAttendance";
 import Login from "./Component/login/login";
-import { TAB_CONSTANTS } from "./constants";
+import {TAB_CONSTANTS} from "./constants";
 import ViewClassSession from "./Component/viewClass/viewClassSession";
 import AddStudent from "./Component/addStudent/addStudent";
 import ViewAttendance from "./Component/ViewAttendance/viewAttendance";
 
 export default class App extends React.Component {
-  state = {
-    authenticated: false,
-    selectedTab: TAB_CONSTANTS.TAKE_ATTENDANCE,
-    admin: false,
-    name: "",
-    referenceKey: ""
-  };
-
-  render() {
-    return (
-      <div className="App" height="100%">
-        <Router>
-          <NavigationBar
-            selectedTab={this.state.selectedTab}
-            setTabSelected={this.setTabSelected.bind(this)}
-            authenticated={this.state.authenticated}
-            username={this.state.name}
-            admin={this.state.admin}
-          />
-          <Route path="/login" render={this.renderLogin} />
-          <Route path="/dashboard" render={this.renderDashboard} />
-          {this.state.authenticated ? (
-            <Redirect to="/dashboard" />
-          ) : (
-            <Redirect to="/login" />
-          )}
-        </Router>
-      </div>
-    );
-  }
-
-  onLogin = userDetails => {
-    if (userDetails.name != null) {
-      console.log(userDetails);
-      this.setState({
-        name: userDetails.name,
-        referenceKey: userDetails.referenceKey,
-        admin: userDetails.admin,
-        authenticated: true
-      });
-    } else {
-      this.setState({
-        authenticated: false
-      });
-    }
-  };
-
-  renderDashboard = () => {
-    if (!this.state.authenticated) return <Redirect to="/login" />;
-    switch (this.state.selectedTab) {
-      case TAB_CONSTANTS.TAKE_ATTENDANCE:
-        return <TakeAttendance matriculationNo={this.state.referencekey} />;
-      case TAB_CONSTANTS.VIEW_ATTENDANCE:
-        return <ViewAttendance matriculationNo={this.state.referenceKey} />;
-      case TAB_CONSTANTS.VIEW_SESSION:
-        return <ViewClassSession />;
-      case TAB_CONSTANTS.ADD_STUDENT:
-        return <AddStudent />;
-    }
-  };
-
-  renderLogin = () => <Login onLogin={this.onLogin.bind(this)} />;
-
-  setTabSelected(tab) {
-    return () => {
-      this.setState({ selectedTab: tab });
+    state = {
+        authenticated: false,
+        selectedTab: TAB_CONSTANTS.TAKE_ATTENDANCE,
+        admin: false,
+        name: "",
+        referenceKey: ""
     };
-  }
+
+    render() {
+        return (
+            <div className="App" height="100%">
+                <Router>
+                    <NavigationBar
+                        selectedTab={this.state.selectedTab}
+                        setTabSelected={this.setTabSelected.bind(this)}
+                        authenticated={this.state.authenticated}
+                        username={this.state.name}
+                        admin={this.state.admin}
+                    />
+                    <Route path="/login" render={this.renderLogin}/>
+                    <Route path="/dashboard" render={this.renderDashboard}/>
+                    {this.state.authenticated ? (
+                        <Redirect to="/dashboard"/>
+                    ) : (
+                        <Redirect to="/login"/>
+                    )}
+                </Router>
+            </div>
+        );
+    }
+
+    onLogin = userDetails => {
+        if (userDetails.status == false) {
+            this.setState({
+                authenticated: false
+            });
+        }
+        else if (userDetails.name != null) {
+            console.log(userDetails);
+            this.setState({
+                name: userDetails.name,
+                referenceKey: userDetails.referenceKey,
+                admin: userDetails.admin,
+                authenticated: true
+            });
+        } else {
+            this.setState({
+                authenticated: false
+            });
+        }
+    };
+
+    renderDashboard = () => {
+        if (!this.state.authenticated) return <Redirect to="/login"/>;
+        switch (this.state.selectedTab) {
+            case TAB_CONSTANTS.TAKE_ATTENDANCE:
+                return <TakeAttendance matriculationNo={this.state.referenceKey} admin={this.state.admin}/>;
+            case TAB_CONSTANTS.VIEW_ATTENDANCE:
+                return <ViewAttendance matriculationNo={this.state.referenceKey}/>;
+            case TAB_CONSTANTS.VIEW_SESSION:
+                return <ViewClassSession/>;
+            case TAB_CONSTANTS.ADD_STUDENT:
+                return <AddStudent/>;
+        }
+    };
+
+    renderLogin = () => <Login onLogin={this.onLogin.bind(this)}/>;
+
+    setTabSelected(tab) {
+        return () => {
+            this.setState({selectedTab: tab});
+        };
+    }
 }
--- client/src/Component/takeAttendance/takeAttendance.js
+++ client/src/Component/takeAttendance/takeAttendance.js
@@ -6,117 +6,159 @@ import canvas from "canvas";
 const faceapi = require("face-api.js");
 
 class TakeAttendance extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      matriculationNo: props.matriculationNo,
-      course: "",
-      class: "",
-      image: null,
-      showCamera: true
+    constructor(props) {
+        super(props);
+        this.state = {
+            matriculationNo: props.matriculationNo,
+            course: "",
+            class: "",
+            image: null,
+            showCamera: true,
+            student: ""
+        };
+        this.webcamRef = React.createRef();
+    }
+
+    render() {
+        const admin = this.props.admin
+        return (
+            <div className="attendance-container">
+                <div className="attendance-header">Input your Course Code</div>
+                <input
+                    className="attendance-input"
+                    type="text"
+                    value={this.state.course}
+                    onChange={e => this.setState({course: e.target.value})}
+                />
+                <div className="attendance-header">Input your Class Code</div>
+                <input
+                    className="attendance-input"
+                    type="text"
+                    value={this.state.class}
+                    onChange={e => this.setState({class: e.target.value})}
+                />
+                {!admin && this.state.showCamera ? (
+                    <Webcam
+                        audio={false}
+                        height={300}
+                        ref={this.webcamRef}
+                        screenshotFormat="image/jpeg"
+                        width={500}
+                        videoConstraints={{width: 500, height: 300, facingMode: "user"}}
+                    />
+                ) : (
+                    <img id="photo1" src={this.state.image}/>
+                )}
+                {!admin && (this.state.showCamera ? (
+                    <div className="camera-btn" onClick={this.webcamCapture}>
+                        Take Photo
+                    </div>
+                ) : (
+                    <div className="camera-btn" onClick={this.takeAnother}>
+                        Retake Photo
+                    </div>
+                ))}
+                {admin && <div className="attendance-header">Input Student Matric No.</div>}
+                {admin && <input
+                    className="attendance-input"
+                    type="text"
+                    value={this.state.student}
+                    onChange={e => this.setState({student: e.target.value})}
+                />
+                }
+                <div className="attendance-btn" onClick={!admin ? this.onBtnClick : this.onAdminClick}>
+                    Submit
+                </div>
+            </div>
+        );
+    }
+
+    onBtnClick = () => {
+        console.log("click");
+        const {course} = this.state;
+        this.generateDescriptors()
+            .then(res => {
+                console.log(JSON.stringify(res.descriptor));
+                fetch("http://localhost:5000/api/attendance/update/", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        classDetail: {
+                            courseIndex: this.state.course,
+                            classIndex: this.state.class
+                        },
+                        descriptor: {
+                            _descriptors: res.descriptor,
+                            _label: this.state.matriculationNo
+                        }
+                    })
+                })
+                    .then(res => res.json())
+                    .then(res => {
+                        if (!res) {
+                            alert("Photo does not match you. Please try again or approach administrator")
+                        } else if (res === true) {
+                            alert("Attendance has been recorded")
+                        }
+                    });
+            })
+            .catch(e => alert("Photo not valid. Please retake and try again or approach administrator"));
     };
-    this.webcamRef = React.createRef();
-  }
 
-  render() {
-    return (
-      <div className="attendance-container">
-        <div className="attendance-header">Input your Course Code</div>
-        <input
-          className="attendance-input"
-          type="text"
-          value={this.state.course}
-          onChange={e => this.setState({ course: e.target.value })}
-        />
-        <div className="attendance-header">Input your Class Code</div>
-        <input
-          className="attendance-input"
-          type="text"
-          value={this.state.class}
-          onChange={e => this.setState({ class: e.target.value })}
-        />
-        {this.state.showCamera ? (
-          <Webcam
-            audio={false}
-            height={300}
-            ref={this.webcamRef}
-            screenshotFormat="image/jpeg"
-            width={500}
-            videoConstraints={{ width: 500, height: 300, facingMode: "user" }}
-          />
-        ) : (
-          <img id="photo" src={this.state.image} />
-        )}
-        {this.state.showCamera ? (
-          <div className="camera-btn" onClick={this.webcamCapture}>
-            Take Photo
-          </div>
-        ) : (
-          <div className="camera-btn" onClick={this.takeAnother}>
-            Retake Photo
-          </div>
-        )}
-        <div className="attendance-btn" onClick={this.onBtnClick}>
-          Submit
-        </div>
-      </div>
-    );
-  }
 
-  onBtnClick = () => {
-    console.log("click");
-    const { course } = this.state;
-    this.generateDescriptors()
-      .then(res => {
-        console.log(JSON.stringify(res.descriptor));
-        fetch("http://localhost:5000/api/attendance/update/", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({
-            classDetail: {
-              courseIndex: this.state.course,
-              classIndex: this.state.class
+    onAdminClick = () => {
+        console.log("click");
+        fetch("http://localhost:5000/api/attendance/admin-update/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
             },
-            descriptor: {
-              _descriptors: res.descriptor,
-              _label: this.state.matriculationNo
-            }
-          })
+            body: JSON.stringify({
+                classDetail: {
+                    courseIndex: this.state.course,
+                    classIndex: this.state.class
+                },
+                matriculationNo: this.state.student
+            })
         })
-          .then(res => res.json())
-          .then(res => console.log(res));
-      })
-      .catch(e => console.log(e));
-  };
+            .then(res => res.json())
+            .then(res => {
+                if (!res) {
+                    alert("There was an error")
+                } else if (res === true) {
+                    alert("Attendance has been recorded")
+                }
+            });
+    }
 
-  webcamCapture = async () => {
-    const imageSrc = this.webcamRef.current.getScreenshot();
-    this.setState({ image: imageSrc, showCamera: false });
-  };
+    webcamCapture = async () => {
+        const imageSrc = this.webcamRef.current.getScreenshot();
+        this.setState({image: imageSrc, showCamera: false});
+    };
 
-  generateDescriptors = async () => {
-    await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
-    await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
-    await faceapi.nets.faceRecognitionNet.loadFromUri("/models");
-    await faceapi.nets.faceExpressionNet.loadFromUri("/models");
-    // let image = document.getElementById("photo");
-    let result = await faceapi
-      .detectSingleFace(
-        "photo",
-        new faceapi.SsdMobilenetv1Options({ minConfidence: 0.5 })
-      )
-      .withFaceLandmarks()
-      .withFaceExpressions()
-      .withFaceDescriptor();
-    console.log(result);
-    return result;
-  };
+    generateDescriptors = async () => {
+        await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
+        await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
+        await faceapi.nets.faceRecognitionNet.loadFromUri("/models");
+        await faceapi.nets.faceExpressionNet.loadFromUri("/models");
+        // let image = document.getElementById("photo");
+        let result = await faceapi
+            .detectSingleFace(
+                "photo1",
+                new faceapi.SsdMobilenetv1Options({minConfidence: 0.5})
+            )
+            .withFaceLandmarks()
+            .withFaceExpressions()
+            .withFaceDescriptor();
+        console.log(result);
+        return result;
+    };
 
-  takeAnother = () => {
-    this.setState({ showCamera: true });
-  };
+    takeAnother = () => {
+        this.setState({showCamera: true});
+    };
 }
 
 export default TakeAttendance;
